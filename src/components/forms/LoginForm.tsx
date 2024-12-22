7e21c3bd42008d4faa98020b1d8b478fa0c40895
OmarMallouk <mr.mallouk.om@gmail.com>
2024-12-22 19:27:53 +0200

    added more in the form

--- src/components/forms/LoginForm.tsx
+++ src/components/forms/LoginForm.tsx
@@ -4,3 +4,7 @@ interface LoginFormProps{
     onLogin: (email: string, password: string) => void;
 }
 
+const LoginForm: React.FC<LoginFormProps> = ({onLogin}) =>{
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+}
