5a71324584e937bbfb32f3db06ab3812110b21dd
OmarMallouk <mr.mallouk.om@gmail.com>
2024-12-23 00:17:46 +0200

    added a private route component

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,9 +1,18 @@
+import { useNavigate } from "react-router-dom";
 
 
 const Home = () => {
+    const navigate = useNavigate();
+
+    const handleLogout = () =>{
+        localStorage.removeItem("Token");
+        navigate("/")
+    }
     return ( 
         <div>
             Welcome Home!
+
+            <button onClick={handleLogout}>logout</button>
         </div>
      );
 }
