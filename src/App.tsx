0f02d60a8636df8e92dd0bff536e18bf7e7677c1
OmarMallouk <mr.mallouk.om@gmail.com>
2024-12-23 00:20:03 +0200

    added the private to the app routes

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { AuthProvider } from "./context/AuthContext";
 import Login from "./pages/Login/Login";
 import Home from "./pages/Home/Home";
 import { BrowserRouter,Routes,Route } from "react-router-dom";
+import PrivateRoute from "./components/PrivateRoutes";
 import "./App.css";
 
 const App: React.FC =() => {
@@ -10,7 +11,8 @@ const App: React.FC =() => {
       <BrowserRouter>
       <Routes>
       <Route path="/" element={<Login/>}/>
-      <Route path="/home" element={<Home/>}/>
+
+      <Route path="/home" element={<PrivateRoute><Home/></PrivateRoute>}/>
       </Routes>
       </BrowserRouter>
     </AuthProvider>
