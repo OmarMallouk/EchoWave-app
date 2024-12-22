af653efe6fceccf9e280ecf6fb5efc02ff3ab849
OmarMallouk <mr.mallouk.om@gmail.com>
2024-12-22 20:25:45 +0200

    added email submit

--- src/components/forms/LoginForm.tsx
+++ src/components/forms/LoginForm.tsx
@@ -7,4 +7,26 @@ interface LoginFormProps{
 const LoginForm: React.FC<LoginFormProps> = ({onLogin}) =>{
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
+
+    const handleSubmit = (e: React.FormEvent) =>{
+        e.preventDefault();
+
+        onLogin(email, password);
+    };
+
+
+    return(
+        <form onSubmit={handleSubmit}>
+            <div>
+                <label htmlFor="email">Email</label>
+                <input 
+                type="email"
+                id="email"
+                value={email}
+                onChange={(e)=> setEmail(e.target.value)}
+                required
+                ></input>
+            </div>
+        </form>
+    )
 }
