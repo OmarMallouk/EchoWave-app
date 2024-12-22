2ac71731d8683643653a54f126dfeaa106f9b153
OmarMallouk <mr.mallouk.om@gmail.com>
2024-12-22 20:41:52 +0200

    added a login Page

---
+++ src/pages/Login/Login.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import LoginForm from "../../components/forms/LoginForm";
+
+
+const Login: React.FC = () =>{
+
+    const handleLogin = async (email: string, password: string) =>{
+
+        try {
+            
+        } catch (error) {
+            console.error("Error login", error);
+            
+        }
+    }
+}
