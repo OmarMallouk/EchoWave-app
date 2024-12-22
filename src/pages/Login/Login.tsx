148fc65775549c0d356c924d721e57deb7231df3
OmarMallouk <mr.mallouk.om@gmail.com>
2024-12-22 20:48:53 +0200

    added more logic

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -1,16 +1,29 @@
 import React from "react";
 import LoginForm from "../../components/forms/LoginForm";
-
+import axios from "axios";
 
 const Login: React.FC = () =>{
 
     const handleLogin = async (email: string, password: string) =>{
 
         try {
+            const response = await Login(email,password);
+            console.log("Login successful", response);
             
         } catch (error) {
             console.error("Error login", error);
             
         }
-    }
+    };
+
+    return (
+
+        <div>
+            <h1>Login</h1>
+            <LoginForm onLogin={handleLogin}/>
+        </div>
+    );
+
 }
+
+export default Login;
