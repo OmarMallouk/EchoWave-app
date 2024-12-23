56a305fe22a3dd008d41653980cfed220f537e42
OmarMallouk <mr.mallouk.om@gmail.com>
2024-12-23 02:10:08 +0200

    added logic in the register page

--- src/pages/Register/Register.tsx
+++ src/pages/Register/Register.tsx
@@ -1,8 +1,25 @@
 import React from "react";
 import RegisterForm from "../../components/forms/RegisterForm";
+import { register } from "../../apis/authApi";
+import { useNavigate } from "react-router-dom";
 
 
 const Register: React.FC = () =>{
+    const navigate = useNavigate();
+
+    const handleRegister = async (username: string, email: string, password: string) =>{
+
+        try {
+            
+            const response = await register(username,email,password);
+            console.log("Registerd successfully", response);
+            navigate("/login");
+            
+        } catch (error) {
+            console.error("Registration failed..", error);
+            
+        }
+    }
 
     
 }
