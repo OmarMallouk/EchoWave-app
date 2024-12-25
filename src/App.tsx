1f7f2163e94347968e2afa2601c570b67eec5b79
OmarMallouk <mr.mallouk.om@gmail.com>
2024-12-25 02:21:15 +0200

    made some changes in app

--- src/App.tsx
+++ src/App.tsx
@@ -4,20 +4,25 @@ import Register from "./pages/Register/Register";
 import Home from "./pages/Home/Home";
 import Navbar from "./components/Navbar/Navbar";
 import { BrowserRouter,Routes,Route } from "react-router-dom";
-import PrivateRoute from "./components/PrivateRoutes";
+import ProtectedRoutes from "./components/ProtectedRoutes";
+import '@mantine/core/styles.css';
+import { MantineProvider } from "@mantine/core";
 import "./App.css";
 
 const App: React.FC =() => {
   return (
+  
     <AuthProvider>
       <BrowserRouter>
+      <MantineProvider>
       <Navbar/>
       <Routes>
-      <Route path="/"/>
+      {/* <Route path="/" element={<PrivateRoute><Home /></PrivateRoute>}/> */}
       <Route path="/login" element={<Login/>}/>
       <Route path="/register" element={<Register/>}/>
-      <Route path="/home" element={<PrivateRoute><Home/></PrivateRoute>}/>
+      <Route path="/home" element={<Home/>}/>
       </Routes>
+      </MantineProvider>
       </BrowserRouter>
     </AuthProvider>
   );
