0d29ce2b0eaff69e7331934455677da8d1aa0e1d
OmarMallouk <mr.mallouk.om@gmail.com>
2024-12-25 04:02:48 +0200

    added the hero section

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,20 +1,22 @@
 import { useNavigate } from "react-router-dom";
 import styles from "./Home.module.css"
+import { Button, Container, Overlay, Text, Title } from '@mantine/core';
+
 
 
 const Home = () => {
-    const navigate = useNavigate();
+    
 
-    const handleLogout = () =>{
-        localStorage.removeItem("Token");
-        navigate("/")
-    }
+  
     return ( 
-        <div className={styles.body2}>
-            Welcome Home!
-
-            <button onClick={handleLogout}>logout</button>
-        </div>
+        <div className={styles.hero}>
+        <Overlay
+          gradient="linear-gradient(180deg, rgba(0, 0, 0, 0.25) 0%, rgba(0, 0, 0, .65) 40%)"
+          opacity={1}
+          zIndex={0}
+        />
+   
+      </div>
      );
 }
  
