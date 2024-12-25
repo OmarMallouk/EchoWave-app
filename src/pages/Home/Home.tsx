d3195add4c497b674b96019e11235bdcfa741a9d
OmarMallouk <mr.mallouk.om@gmail.com>
2024-12-25 20:41:12 +0200

    added more html

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -81,7 +81,14 @@ const Home = () => {
         <Container className={styles.container} size="xl">
           <Title className={styles.title}>Unique Exclusive <span className={styles.highlightLyrics}>Lyrics</span>, <br />
           <span className={styles.subtitle}>Just for <span className={styles.highlightYou}>You</span></span></Title>
-        
+          <Title className={styles.description} size="sm" mt="xl">
+          Transform your ideas into original <span className={styles.highlightMagic}>lyrics</span> with the power of AI. Whether you’re a budding <span className={styles.highlightMagic}>songwriter</span> or a seasoned <span className={styles.highlightMagic}>musician</span>, our platform helps you craft songs that reflect your unique <span className={styles.highlightMagic}>voice</span>, <span className={styles.highlightMagic}>style</span> and <span className={styles.highlightMagic}>mood</span>, instantly. 
+          </Title>
+          <div className={styles.heroWrap} >
+              <Title className={styles.intro1} size="sm" mt="xl">
+               Not a member yet ?
+              </Title>
+             
           </div>
         </Container>
       </div>
