e53a4645ffa7031f47286ab0f0f808975ad7aa84
OmarMallouk <mr.mallouk.om@gmail.com>
2024-12-27 23:13:24 +0200

    added an a tag

--- src/pages/Originality/Originality.tsx
+++ src/pages/Originality/Originality.tsx
@@ -88,7 +88,13 @@ const Originality = () => {
         data-twe-dropdown-item-ref
         >Another action</a>
     </li>
-   
+    <li>
+      <a
+        className="block w-full whitespace-nowrap bg-white px-4 py-2 text-sm font-normal text-neutral-700 hover:bg-zinc-200/60 focus:bg-zinc-200/60 focus:outline-none active:bg-zinc-200/60 active:no-underline dark:bg-surface-dark dark:text-white dark:hover:bg-neutral-800/25 dark:focus:bg-neutral-800/25 dark:active:bg-neutral-800/25"
+        href="#"
+        data-twe-dropdown-item-ref
+        >Something else here</a>
+    </li>
   </ul>
 </div>
       
