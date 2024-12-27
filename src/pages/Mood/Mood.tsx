ba7712e97170f1d74153c249ce7e6534799c0530
OmarMallouk <mr.mallouk.om@gmail.com>
2024-12-27 06:00:54 +0200

    added a ul

--- src/pages/Mood/Mood.tsx
+++ src/pages/Mood/Mood.tsx
@@ -64,6 +64,19 @@ const Mood = () => {
       </div>
    
    
+<div className={styles.Title1}> <h1>Didn't find any? search more </h1></div>  
+
+
+<div className="dropdown dropdown-hover dropdown-right dropdown-end">
+  <div tabIndex={0} role="button" className="btn m-1">Moods</div>
+  <ul tabIndex={0} className="dropdown-content menu bg-base-100 rounded-box z-[1] w-52 p-2 shadow">
+    <li><a>Item 1</a></li>
+    <li><a>Item 2</a></li>
+  </ul>
+</div>
+   
+
+
 
 
 
