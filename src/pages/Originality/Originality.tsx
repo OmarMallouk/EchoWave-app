dc3850735b03fe5c649b8e4f68855fda4760e64d
OmarMallouk <mr.mallouk.om@gmail.com>
2024-12-27 23:10:08 +0200

    added originality page

---
+++ src/pages/Originality/Originality.tsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import { useState, useEffect } from 'react';
+import { TextInput  } from '@mantine/core';
+import styles from './Originalty.module.css';
+import {
+    Dropdown,
+    Ripple,
+    initTWE,
+  } from "tw-elements";
+
+
+
+const Originality = () => {
+    const [focused, setFocused] = useState(false);
+
+
+    return ( 
+
+        <div className={styles.body4}>
+
+
+
+
+
+
+
+  
+
+
+
+
+</div>
+     );
+}
+ 
+export default Originality;
