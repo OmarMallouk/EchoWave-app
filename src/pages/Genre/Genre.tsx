6d25243b75654ca9e1df5c9eaf1af8bdbec0918d
OmarMallouk <mr.mallouk.om@gmail.com>
2024-12-27 15:09:15 +0200

    added some states

--- src/pages/Genre/Genre.tsx
+++ src/pages/Genre/Genre.tsx
@@ -1,8 +1,31 @@
+import React from "react";
+import { useState, useEffect } from "react";
+import styles from "./Genre.module.css"
+import gsap from "gsap";
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
+
+gsap.registerPlugin(ScrollTrigger);
+
+
 const Genre = () => {
+
+  const [selectedMood, setSelectedMood] = useState('');
+  const [aiOutput, setAiOutput] = useState('');
+
+    useEffect(() => {
+        const images = document.querySelectorAll(`.${styles.image}`);
+      
+     
+      
+      }
+    
     return ( 
 
-        <div>
-            <h1>hi</h1>
+
+    <div className={styles.body3}>
+
+
+
         </div>
      );
 }
