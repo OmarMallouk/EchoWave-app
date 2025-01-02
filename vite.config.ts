2f2e2d52b64e80376c8314e559d29f879f55be5f
OmarMallouk <mr.mallouk.om@gmail.com>
2025-01-03 01:52:34 +0200

    adjusted vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,14 @@
 import path from "path"
 import { defineConfig } from 'vite'
+import { visualizer } from "rollup-plugin-visualizer"
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), visualizer({
+    filename: './dist/stats.html', 
+    open: true,                 
+  })],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
