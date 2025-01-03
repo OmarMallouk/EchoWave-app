a66bcf176c84c47bbec76b2bbc4c0600bb46d321
OmarMallouk <mr.mallouk.om@gmail.com>
2025-01-03 03:56:03 +0200

    added new channel page

---
+++ src/pages/Channel/Channel.tsx
@@ -0,0 +1,11 @@
+import styles from "./Channel.module.css";
+
+const Channel = () => {
+    return ( 
+        <div className={styles.body6}>
+
+        </div>
+     );
+}
+ 
+export default Channel;
