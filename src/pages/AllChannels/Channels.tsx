00fb6ef4e61ea5d2d21a3a5f8b66d8364b4c746d
OmarMallouk <mr.mallouk.om@gmail.com>
2025-01-03 03:47:44 +0200

    added channels pagee and imports

--- src/pages/AllChannels/Channels.tsx
+++ src/pages/AllChannels/Channels.tsx
@@ -1,7 +1,20 @@
+import React ,{ useState, useEffect } from "react";
+import styles from "./Channels.module.css";
+import { url } from "inspector";
+import originalCh from "/assests/originalCheck.jpg?url";
+import writing from "/assests/writing.jpg?url";
+import channel1 from "/assests/records5.jpeg?url";
+
+
 const Channels = () => {
+
+  
     return ( 
-        <div>
-            hi
+        <div className={styles.body5}>
+
+
+    </div>
+
         </div>
      );
 }
