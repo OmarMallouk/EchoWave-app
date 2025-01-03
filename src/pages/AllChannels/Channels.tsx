83f9bd6f6d4df5e889e8057d07377d5a7f9ef16a
OmarMallouk <mr.mallouk.om@gmail.com>
2025-01-03 03:47:55 +0200

    added some mock data

--- src/pages/AllChannels/Channels.tsx
+++ src/pages/AllChannels/Channels.tsx
@@ -5,6 +5,20 @@ import originalCh from "/assests/originalCheck.jpg?url";
 import writing from "/assests/writing.jpg?url";
 import channel1 from "/assests/records5.jpeg?url";
 
+const popularData = [
+    { id: 1, title: "Channel One", image: originalCh },
+    { id: 2, title: "Channel Two", image: writing },
+    { id: 3, title: "Channel Three", image: channel1 },
+    { id: 4, title: "Channel Three", image: channel1 },
+    { id: 4, title: "Channel Three", image: channel1 },
+    { id: 4, title: "Channel Three", image: channel1 },
+  ];
+
+  const data = [
+    { channel: channel1, song: "Song A", views: "1.2M" },
+    { channel: channel1, song: "Song B", views: "850K" },
+    { channel: channel1, song: "Song C", views: "540K" },
+  ];
 
 const Channels = () => {
 
