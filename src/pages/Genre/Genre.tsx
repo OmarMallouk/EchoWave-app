1924f6de20be09d0f596dd831b7b96401f9bc6c4
OmarMallouk <mr.mallouk.om@gmail.com>
2025-01-06 20:43:16 +0200

    made adjustments the gsap useEffect

--- src/pages/Genre/Genre.tsx
+++ src/pages/Genre/Genre.tsx
@@ -15,6 +15,32 @@ const Genre = () => {
     const [user, setUser] = useState('');
     const [error, setError] = useState("");
 
+    useEffect(() => {
+      const images = document.querySelectorAll(`.${styles.image}`);
+    
+      images.forEach((image) => {
+        gsap.fromTo(
+          image,
+          { 
+            y: gsap.utils.random(100, 50),
+            x: gsap.utils.random(-50, 50),
+          },
+          {
+            y: 0, 
+            x: 0,
+            stagger: 0.5,
+            scrollTrigger: {
+              trigger: image,
+              start: "top 80%",
+              end: "bottom 20%",
+              scrub: 1,
+              markers: false,
+            },
+          }
+        );
+      });
+    }, []);
+      
 
   
   
