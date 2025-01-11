4fca1da6713a5cc8649082794c7784043925f073
OmarMallouk <mr.mallouk.om@gmail.com>
2025-01-12 00:54:20 +0200

    added multiple states for later use

--- src/pages/Channel/Channel.tsx
+++ src/pages/Channel/Channel.tsx
@@ -1,8 +1,37 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
 import styles from "./Channel.module.css";
+import channel4 from "/assests/writing.jpg?url";
+import { Lyric, UserLyrics, Song } from "@/lib/Types";
 
 const Channel = () => {
+    const [userId, setUserId] = useState('');
+    const [userLyrics, setUserLyrics] = useState<Lyric[]>([]);
+    const [selectedSong, setSelectedSongs] = useState<Song | null>(null);
+    const [title, setTitle] = useState('');
+    const [lyrics, setLyrics] = useState(null); 
+    const [showModal, setShowModal] = useState(false); 
+    const [selectedForMerge, setSelectedForMerge] = useState<string[]>([]);
+    const [songs, setSongs] = useState<Song[]>([]);
+    const [isSelecting, setIsSelecting] = useState(false);
+    const [comments, setComments] = useState<Song["comments"]>([]);
+    const [newComment, setNewComment] = useState("");
+    const [error, setError] = useState("");
+
+
+
+
+
+
+          
     return ( 
         <div className={styles.body6}>
+   
+
+
+
+
+
 
         </div>
      );
