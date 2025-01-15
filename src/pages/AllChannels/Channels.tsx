6b027a663b0fa0d7bb7e04e2996a6f40818a2896
OmarMallouk <mr.mallouk.om@gmail.com>
2025-01-15 19:05:28 +0200

    added some logic

--- src/pages/AllChannels/Channels.tsx
+++ src/pages/AllChannels/Channels.tsx
@@ -1,27 +1,33 @@
 import React ,{ useState, useEffect } from "react";
 import styles from "./Channels.module.css";
 import { url } from "inspector";
+import { Lyric,  Song, User } from "@/lib/Types";
+import axios from "axios";
 import originalCh from "/assests/originalCheck.jpg?url";
 import writing from "/assests/writing.jpg?url";
 import channel1 from "/assests/records5.jpeg?url";
-
-const popularData = [
-    { id: 1, title: "Channel One", image: originalCh },
-    { id: 2, title: "Channel Two", image: writing },
-    { id: 3, title: "Channel Three", image: channel1 },
-    { id: 4, title: "Channel Three", image: channel1 },
-    { id: 4, title: "Channel Three", image: channel1 },
-    { id: 4, title: "Channel Three", image: channel1 },
-  ];
-
-  const data = [
-    { channel: channel1, song: "Song A", views: "1.2M" },
-    { channel: channel1, song: "Song B", views: "850K" },
-    { channel: channel1, song: "Song C", views: "540K" },
-  ];
+import { Link } from "react-router-dom";
 
 const Channels = () => {
+  const [userId, setUserId] = useState('');
+  const[channels, setChannels] = useState('');
+  const [users, setUsers] = useState<User[]>([]);
 
+
+
+
+       useEffect(() => {
+        const userData = JSON.parse(localStorage.getItem("User") || "{}");
+           if (userData._id) {
+                 setUserId(userData._id);
+               }
+           }, []);
+  
+
+
+
+
+  const songProducers = users.filter((user) => user.role === "song_producer");
   
     return ( 
         <div className={styles.body5}>
@@ -30,39 +36,28 @@ const Channels = () => {
             
 
 <div className={styles.cardGrid}>
-      {popularData.map((item) => (
-        <div key={item.id} className={styles.card}>
-          <img src={item.image} alt={item.title} className={styles.cardImage} />
-          <h3 className={styles.cardTitle}>{item.title}</h3>
+      {songProducers.map((item) => (
+         <Link 
+         key={item._id} 
+         to={`/singleChannel/${item._id}`}
+         state={{ item }} 
+         className={styles.card}
+       >
+        <div key={item._id} className={styles.card}>
+          <img
+  src={item.profile_picture ? `http://localhost:8080${item.profile_picture}` : channel1}
+  alt={item.channelName || "Default Channel Name"}
+  className={styles.cardImage}
+/>
+          <h3 className={styles.cardTitle}>{item.channelName}</h3>
         </div>
+        </Link>
       ))}
+     
     </div>
 
 
-    
-    <div className={styles.Title3}> <h1>Most Viewed</h1></div>
 
-
-
-    <div className={styles.tableGrid}>
-      <div className={styles.columnHeader}>Channel</div>
-      <div className={styles.columnHeader}>Song Name</div>
-      <div className={styles.columnHeader}>Views</div>
-      
-      {data.map((item, index) => (
-        <React.Fragment key={index}>
-          <div className={styles.gridItem}>
-            <img
-              src={item.channel}
-              alt={item.channel}
-              className={styles.channelImage}
-            />
-          </div>
-          <div className={styles.gridItem}>{item.song}</div>
-          <div className={styles.gridItem}>{item.views}</div>
-        </React.Fragment>
-      ))}
-    </div>
 
         </div>
      );
