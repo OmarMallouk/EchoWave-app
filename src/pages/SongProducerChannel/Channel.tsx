b842620ab033a58b8472465e3cca58609f807e0a
OmarMallouk <mr.mallouk.om@gmail.com>
2025-01-15 19:19:00 +0200

    added channel page

---
+++ src/pages/SongProducerChannel/Channel.tsx
@@ -0,0 +1,64 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+import styles from "./Channel.module.css";
+import channel4 from "/assests/writing.jpg?url";
+import { Lyric,  Song, User } from "@/lib/Types";
+import { Link } from "react-router-dom";
+
+const Channel = () => {
+   const [user, setUser] = useState<User>({
+    channelName: '',
+    description: '',
+    username: '',
+    profile_picture: '',
+    _id: '',
+    role:'',
+    bookmarkedChannels: [],
+    });
+    const [userId, setUserId] = useState('');
+    const [userLyrics, setUserLyrics] = useState<Lyric[]>([]);
+    const [selectedSong, setSelectedSongs] = useState<Song | null>(null);
+    const [title, setTitle] = useState('');
+    const [lyrics, setLyrics] = useState(null); 
+    const [showModal, setShowModal] = useState(false); 
+    const [selectedForMerge, setSelectedForMerge] = useState<string[]>([]);
+    const [songs, setSongs] = useState<Song[]>([]);
+    const [newSong, setNewSong] = useState("");
+    const [isSelecting, setIsSelecting] = useState(false);
+    const [comments, setComments] = useState<Song["comments"]>([]);
+    const [newComment, setNewComment] = useState("");
+    const [isModalOpen, setIsModalOpen] = useState(false);
+    const [formData, setFormData] = useState({
+      channelName: "",
+      description: "",
+    });
+    const [profilePicture, setProfilePicture] = useState<File | null>(null);
+    const [previousData, setPreviousData] = useState(null);
+    const [error, setError] = useState("");
+
+
+    useEffect(() => {
+      const userData = JSON.parse(localStorage.getItem("User") || "{}");
+         if (userData._id) {
+               setUserId(userData._id);
+             }
+         }, []);
+
+
+        //  useEffect(() => {
+        //   const userData = JSON.parse(localStorage.getItem("User") || "{}");
+        //      if (userData._id) {
+        //            setUserId(userData._id);
+        //          }
+        //      }, []);
+      
+
+    return ( 
+        <div className={styles.body6}>
+      
+ 
+        </div>
+     );
+}
+ 
+export default Channel;
